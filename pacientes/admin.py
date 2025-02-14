1945cb578439099fc9b93a2bcfeabcc757453489
jose <jose@DESKTOP-IILTP2S>
2025-02-14 15:19:29 -0300

    aa

--- pacientes/admin.py
+++ pacientes/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Pacientes
+from .models import Pacientes, Consultas, Tarefas
 
 
 admin.site.register(Pacientes)
+admin.site.register(Consultas)
+admin.site.register(Tarefas)
+
--- pacientes/models.py
+++ pacientes/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from datetime import *
+from django.urls import reverse
+
 
 
 class Pacientes(models.Model):
@@ -18,3 +21,38 @@ class Pacientes(models.Model):
 
     def __str__(self):
         return self.nome
+
+class Tarefas(models.Model):
+    frequencia_choices = (
+        ('D', 'Diário'),
+        ('1S', '1 vez por semana'),
+        ('2S', '2 vezes por semana'),
+        ('3S', '3 vezes por semana'),
+        ('N', 'Ao necessitar')
+    )
+    tarefa = models.CharField(max_length=255)
+    instrucoes = models.TextField()
+    frequencia = models.CharField(max_length=2, choices=frequencia_choices, default='D')
+
+    def __str__(self):
+        return self.tarefa
+    
+class Consultas(models.Model):
+    humor = models.PositiveIntegerField()
+    registro_geral = models.TextField()
+    video = models.FileField(upload_to="video")
+    tarefas = models.ManyToManyField(Tarefas)
+    paciente = models.ForeignKey(Pacientes, on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.paciente.nome
+
+    @property
+    def link_publico(self):
+        return f"http://127.0.0.1:8000{reverse('consulta_publica', kwargs={'id': self.id})}"
+    
+
+class Visualizacoes(models.Model):
+    consulta = models.ForeignKey(Consultas, on_delete=models.CASCADE)
+    ip = models.GenericIPAddressField()
